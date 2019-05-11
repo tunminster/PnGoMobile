0773e22d00d42d94cc8017769a93e3001265c0cf
tminhein <tminhein@gmail.com>
2019-05-11 12:29:35 +0100

    no message

---
+++ screens/ScanScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BarcodeScanner from 'react-native-barcodescanner';
+
+export default class ScanScreen extends React.Component{
+    constructor(props){
+        super (props);
+
+        this.state = {
+            torchMode: 'off',
+            cameraType: 'back',
+        };
+    }
+
+    barcodeReceived(e){
+        console.log('Barcode: ' + e.data);
+        console.log('Type: ' + e.type);
+    }
+
+    render(){
+        return (
+            <BarcodeScanner
+        onBarCodeRead={this.barcodeReceived}
+        style={{ flex: 1 }}
+        torchMode={this.state.torchMode}
+        cameraType={this.state.cameraType}
+      />
+        );
+    }
+}
